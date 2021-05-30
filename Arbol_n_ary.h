d2d17f8a02f30d7a01fd90fb37867525d7e35835
Fausto Lavezzari <faustolavezzari99@gmail.com>
2021-05-30 02:45:36 -0300

    creo que cree el arbol, faltan metodos auxiliares y no se muy bien que comparaciones tener en cuenta

--- Arbol_n_ary.h
+++ Arbol_n_ary.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <sstream> 
 #include "Lista.h"
+#include "Cola.h"
 using namespace std;
 
 
@@ -13,7 +14,7 @@ private:
     Lista<NodoArbol*> hijos;
     int contador;
 public:
-    NodoArbol(string palabra_agregar, NodoArbol *padre_agregar, int n) {
+    NodoArbol(string palabra_agregar, NodoArbol *padre_agregar) {
         palabra = palabra_agregar;
         padre = padre_agregar;
     }
@@ -29,11 +30,55 @@ public:
     int getRepeticiones() {
         return contador;
     }
+    string getPalabra() {
+        return palabra;
+    }
+    void setPalabra(string palabra_modificar) {
+        palabra = palabra_modificar;
+    }
+    Lista<NodoArbol*> getHijos() {
+        return hijos;
+    }
 };
 
-class arbol {
+class Arbol {
 private:
     int hijos_maximos;
+    int comparaciones;
+    Cola<NodoArbol*> proximas_raices;
     NodoArbol* raiz;
+    void swap(NodoArbol*& p1, NodoArbol*& p2);
+    bool esta(NodoArbol* aux, string palabra);
+public:
+    
+    Arbol(int n) {
+        hijos_maximos = n;
+        int comparaciones = 0;
+        raiz = NULL;
+    }
+    
+    void add(string palabra) {
+        NodoArbol* nuevo = new NodoArbol(palabra, raiz);
+        proximas_raices.add(nuevo);
+        if (raiz == NULL) {       
+            raiz = nuevo;
+        }
+        else{
+            bool repetida = esta(palabra);
+            if(!repetida){
+                if (raiz->getHijos().size() != hijos_maximos) {
+                    if (palabra > raiz->getPalabra()) {
+                        swap(nuevo, raiz);
+                    }
+                    raiz->addHijo(nuevo);
+                }
+                else {
+                    raiz = proximas_raices.tope();
+                    proximas_raices.desencolar();
+                }
+            }
+        }
+    }
+    bool esta(string palabra);
 
 };
